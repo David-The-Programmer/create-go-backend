7eadeb97bfa1abb939bd0815c7016929ef077450
David-The-Programmer <42868389+David-The-Programmer@users.noreply.github.com>
2024-05-01 16:56:27 +0800

    fix: Script copies all content of template into user project folder

--- create-go-backend.sh
+++ create-go-backend.sh
@@ -5,10 +5,13 @@ read -p "Enter your go module path: " modulePath
 
 # TODO: Need to fix being unable to use "~" to denote home user directory
 mkdir $folderPath
-cp -r /tmp/create-go-backend/* $folderPath
+# TODO: Exclude the script in the copying
+cp -r /tmp/create-go-backend/. $folderPath
 
 cd $folderPath
 go mod init $modulePath
 
 rm -rf /tmp/create-go-backend
 echo "Go backend project folder created"
+
+# TODO: Make an automated test to make sure all content is copied properly
